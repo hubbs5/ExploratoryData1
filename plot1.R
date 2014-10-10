4faa0797f6441509279f099df2c0eb51a2d40ad0
Christian <christiandhubbs@gmail.com>
2014-10-10 12:36:05 +0200

    Add plot1.R

---
+++ plot1.R
@@ -0,0 +1,52 @@
+#EDA Week 1 Plot 1
+
+#Exploratory Data Analysis Project
+
+#Ensure dates are read in English, not German
+Sys.setlocale("LC_TIME", "English")
+
+#Check for packages
+if (!require("data.table")) {
+  install.packages("data.table")
+}
+
+if (!require("lubridate")) {
+  install.packages("liubridate")
+}
+
+#Apply Packages
+library(data.table)
+library(lubridate)
+
+fileURL <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+download.file(fileURL, destfile = "~/R/Exploratory Data Analysis/household_power_consumption.zip")
+#Unzip and Extract Data
+power_data <- read.table(unz("~/R/Exploratory Data Analysis/household_power_consumption.zip", "household_power_consumption.txt"), header = T, sep = ";")
+
+#Formate dates
+power_data$Date <- dmy(power_data$Date)
+power_data$Date <- as.Date(power_data$Date)
+
+#Subset Data
+data <- subset(power_data, (Date >= as.Date("2007-02-01") & Date <= as.Date("2007-02-02")))
+attach(data) #I can clean out all of the data$xxx with this command
+
+#Ensure Proper Classes
+data$Global_active_power <- as.numeric(as.character(data$Global_active_power))
+data$Global_reactive_power <- as.numeric(as.character(data$Global_reactive_power))
+data$Sub_metering_1 <- as.numeric(as.character(data$Sub_metering_1))
+data$Sub_metering_2 <- as.numeric(as.character(data$Sub_metering_2))
+data$Sub_metering_3 <- as.numeric(as.character(data$Sub_metering_3))
+data$Voltage <- as.numeric(as.character(data$Voltage))
+
+#Create Histogram
+bin = seq(0,6,by = 0.5) #Define number of bins
+t_h = "Global Active Power" #Set the title
+x_lh = "Global Active Power (kilowatts)" #Set the x-axis label
+y_lh = "Frequency"
+hist(data$Global_active_power, ylim = c(0,1200), main = paste(t_h),
+     ylab = y_lh, xlab = x_lh, col ="red", plot = TRUE)
+
+#Create PNG
+dev.copy(png, file="plot1.png", height=480, width=480)
+dev.off()
